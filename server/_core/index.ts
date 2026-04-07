9cf9702afa19c3afbe6f345f9a44b7c8013ed6a2
Muzaib <msunknowngamer@gmail.com>
2026-04-08 02:40:14 +0530

    force vite in production (temp fix)

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -44,7 +44,7 @@ async function startServer() {
     })
   );
   // development mode uses Vite, production mode uses static files
-  if (process.env.NODE_ENV === "development") {
+  if (true) {
     await setupVite(app, server);
   } else {
     serveStatic(app);
